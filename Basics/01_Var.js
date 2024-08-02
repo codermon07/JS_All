72b3ba6c91fa3eebf486f040c77859a03ee4619f
codermon07 <114828944+codermon07@users.noreply.github.com>
2024-08-02 07:17:41 +0000

    commit successfull

---
+++ Basics/01_Var.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "kolhe@gmail.com"
+var accountPassword = "12345"
+accountcity = "Nashik" //bad habit
+let accountState;
+console.log(accountState);
+
+
+/*   
+prefer not to use var because issue in
+block scope and functional scope
+*/ 
+
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "Hc2gmil.com"
+accountPassword ="234493"
+accountcity = "Kerala"
+
+console.table([accountEmail,accountPassword,accountcity]);
+
+
+
+
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs. not browser
+
+console.log(3 + 3)
+console.log("Welcome");
+
+//MDN - JS Doc
+
+let name = "Deepraj"
+let age = 18 
+let isloggedIn = false
+
+// number
+// bigint
+// string = ""
+// boolean
+// null = standalone value -- its an object
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
+
+
+
+
+
+
+
+
+
